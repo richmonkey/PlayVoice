89ddcb8abe972b3ab3acd15c504b72cde8b5e060
houxh <houxuehua49@gmail.com>
2026-06-18 21:55:26 +0800

    服务器接口url

--- ios/GoogleSignInDemo/Infrastructure/Core/AppConfig.swift
+++ ios/GoogleSignInDemo/Infrastructure/Core/AppConfig.swift
@@ -2,10 +2,10 @@ import Foundation
 
 @objcMembers
 final class AppConfig: NSObject {
-    static let apiBaseURL = "http://192.168.1.6:8000"
+//    static let apiBaseURL = "http://192.168.1.6:8000"
 //    static let roomServerBaseURL = "ws://192.168.1.198:4444"
     
-//    static let apiBaseURL = "https://gobelieve.xyz"
+    static let apiBaseURL = "https://gobelieve.xyz"
     static let roomServerBaseURL = "wss://gobelieve.xyz/room"
 
     static let logNetworkRequests = true
